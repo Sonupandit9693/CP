6c0c76de7731f14850459b4b36fc87e6a567bffe
Sonupandit9693 <111635226+Sonupandit9693@users.noreply.github.com>
2022-11-10 13:15:35 +0530

    Add files via upload

---
+++ rectangle.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //print rectangle charec ter format like 'A'and a++ hota rahega.
+       /*int n;
+        cout<<"Enter the number";
+        cin>>n;
+        int i=1;
+        while (i<=n){
+            int j=1;
+            while(j<=n){
+            char ch ='A'+i+j-2;
+            cout<<ch<<" ";
+            j++;
+            }
+            cout<<endl;
+            i++;
+    }*/
+    
+   /*int n;
+    cout<<"Enter the number";
+    cin>>n;
+    int i=1;
+    while (i<=n){
+        int j=1;
+        while(j<=n){
+        char ch ='A'+i-1;
+        cout<<ch<<" ";
+        j++;
+        }
+        cout<<endl;
+        i++;
+    }*/
+    /*int n;
+    cout<<"Enter the number";
+    cin>>n;
+    int i=1;
+    char ch='A';
+    while (i<=n){
+        int j=1;
+        while(j<=n){
+        cout<<ch<<" ";
+        j++;
+        ch++;
+        }
+        cout<<endl;
+        i++;
+    }*/
+    int n;
+    cout<<"Enter the number";
+    cin>>n;
+    int i=1;
+    while (i<=n){
+        int j=1;
+        while(j<=n){
+        char ch='A'+i-1;
+        cout<<ch<<" ";
+        j++;
+        ch++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
